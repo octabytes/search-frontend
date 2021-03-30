cd87370466982f53f39c06edc778e9c99b544f88
axeemhaider <axeemhaider@gmail.com>
2021-03-30 13:15:11 +0500

    update api url

--- src/api/search.js
+++ src/api/search.js
@@ -5,14 +5,14 @@ let apiURL;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   apiURL = "http://localhost:5000";
 } else {
-  apiURL = "https://search-api-dot-islamicnet.appspot.com/";
+  apiURL = "https://search-api-dot-islamicnet.appspot.com/v1";
 }
 
 const API = axios.create({ baseURL: apiURL });
 
 const SearchAPI = async (query) => {
   try {
-    const response = await API.get(`/query/${query}`);
+    const response = await API.get(`/search/${query}`);
     return response.data;
   } catch (err) {
     console.error(err);
